14091d7a55e6ab687a612d978bcef4f503217b04
akbfitra <akbarfitralistyono@gmail.com>
2019-10-03 21:19:33 +0700

    route controller

--- config/axios.js
+++ config/axios.js
@@ -1,32 +1,27 @@
 const axios = require('axios')
 
-function axiosFlight() {
-    return axios.create({
+const axiosFlight = axios.create({
         baseURL: `https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0`,
         headers: {
             "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
             "x-rapidapi-key": `${process.env.FLIGHT_KEY}`
         }
     })
-}
 
-function axiosWeather() {
-    return axios.create({
-        baseURL: `https://api.stormglass.io/v1/weather/point`,
+
+const axiosWeather = axios.create({
+        baseURL: `https://api.stormglass.io/v1/weather`,
         headers: {
             "Authorization": `${process.env.WEATHER_KEY}`
         }
     })
-}
 
-function axiosEvent() {
-    return axios.create({
+const axiosEvent = axios.create({
         baseURL: `https://www.eventbriteapi.com/v3/events/search`,
         headers: {
             "Authorization": `Bearer ${process.env.EVENT_KEY}`
         }
     })
-}
 
 module.exports = {
     axiosFlight,
